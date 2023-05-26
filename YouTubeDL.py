0d7c392eba9b4687c026ff39b2c74614a12aa234
alivergg <114956779+alivergg@users.noreply.github.com>
2023-05-27 01:00:31 +0300

    Add files via upload

---
+++ YouTubeDL.py
@@ -0,0 +1,87 @@
+import os
+import subprocess
+
+from telethon.tl.types import Message
+from yt_dlp import YoutubeDL
+import logging
+
+from .. import loader, utils
+
+
+@loader.tds
+class YouTubeMod(loader.Module):
+    """Download YouTube video and music"""
+
+    strings = {
+        "name": "YouTube",
+        "args": "🎞 <b>You need to specify a link</b>",
+        "downloading": "🎞 <b>Downloading...</b>",
+        "not_found": "🎞 <b>Video not found...</b>",
+    }
+
+    strings_ru = {
+        "args": "🎞 <b>Укажите ссылку на видео</b>",
+        "downloading": "🎞 <b>Скачиваю...</b>",
+        "not_found": "🎞 <b>Видео не найдено...</b>",
+        "_cmd_doc_yt": "[mp3] <ссылка> - Скачать видео YouTube",
+        "_cls_doc": "Скачать YouTube видео",
+    }
+
+    @loader.unrestricted
+    async def ytcmd(self, message: Message):
+        """[mp3] <ссылка> - Скачать видео с YouTube"""
+        args = utils.get_args_raw(message)
+        message = await utils.answer(message, self.strings("downloading"))
+
+        ext = False
+        if len(args.split()) > 1:
+            ext, args = args.split(maxsplit=1)
+
+        if not args:
+            return await utils.answer(message, self.strings("args"))
+
+        ydl_opts = {
+            "outtmpl": "/tmp/%(title)s.%(ext)s",
+            "writethumbnail": True,
+        }
+
+        if ext == "mp3":
+            ydl_opts["postprocessors"] = [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                    "preferredquality": "192",
+                }
+            ]
+
+        with YoutubeDL(ydl_opts) as ydl:
+            try:
+                info = ydl.extract_info(args, download=True)
+            except Exception:
+                await utils.answer(message, self.strings("not_found"))
+                return
+
+            path = ydl.prepare_filename(info)
+
+            if ext == "mp3":
+                filename, ext = os.path.splitext(path)
+                out = f"{filename}.mp3"
+                subprocess.call(
+                    ["ffmpeg", "-y", "-i", path, out],
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.STDOUT,
+                )
+                try:
+                    os.remove(f"{filename}.webp")
+                except:
+                    os.remove(f"{filename}{ext}")
+
+                await self._client.send_file(message.peer_id, out)
+                os.remove(out)
+
+            else:
+                await self._client.send_file(message.peer_id, path)
+                os.remove(path)
+
+        if message.out:
+            await message.delete()
